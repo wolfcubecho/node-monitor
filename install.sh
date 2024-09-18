8875f32f6f7b5a4cb9032941e6184d7e52494fbd
wolfcube <128440852+wolfcubecho@users.noreply.github.com>
2024-09-18 12:09:17 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading Node Monitor setup script..."
-curl -o setup_node_monitor.sh https://raw.githubusercontent.com/wolfcubecho/node-monitor/main/setup_node_monitor.sh
+curl -s -o setup_node_monitor.sh https://raw.githubusercontent.com/wolfcubecho/node-monitor/main/setup_node_monitor.sh
 chmod +x setup_node_monitor.sh
 
 echo "Running setup script..."
